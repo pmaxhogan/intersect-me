795eb193d578edc16f0b74c97c508db6295ceb8d
pmaxhogan <max@maxhogan.dev>
2023-04-09 23:58:46 -0500

    Song cards as links

--- hosting/components/SongCard.tsx
+++ hosting/components/SongCard.tsx
@@ -5,29 +5,36 @@ import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
 import React from "react";
 import {GenericSong} from "../types/api";
+import CardActionArea from "@mui/material/CardActionArea";
 
 export default function SongCard({song}: { song: GenericSong }) {
-    return <Card sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        fontSize: "smaller"
-    }}>
-        <Box sx={{display: "flex", flexDirection: "column", height: 151}}>
-            <CardContent sx={{flex: "1 0 auto"}}>
-                <Typography component="div" variant="subtitle1">
-                    {song.name}
-                </Typography>
-                <Typography variant="subtitle2" color="text.secondary" component="div">
-                    {song.artist}
-                </Typography>
-            </CardContent>
-        </Box>
-        <CardMedia
-            component="img"
-            sx={{width: 151}}
-            image={song.albumArt}
-            alt={song.name + " by " + song.artist}
-        />
+    const openExt = () => {
+        if (song.url) window.open(song.url, "_blank");
+    }
+
+    return <Card>
+        <CardActionArea onClick={openExt} sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            fontSize: "smaller"
+        }}>
+            <Box sx={{display: "flex", flexDirection: "column", height: 151}}>
+                <CardContent sx={{flex: "1 0 auto"}}>
+                    <Typography component="div" variant="subtitle1">
+                        {song.name}
+                    </Typography>
+                    <Typography variant="subtitle2" color="text.secondary" component="div">
+                        {song.artist}
+                    </Typography>
+                </CardContent>
+            </Box>
+            <CardMedia
+                component="img"
+                sx={{width: 151}}
+                image={song.albumArt}
+                alt={song.name + " by " + song.artist}
+            />
+        </CardActionArea>
     </Card>;
 }
